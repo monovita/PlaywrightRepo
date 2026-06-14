a59d6f6c2253c8a525c721a848605234344144c8
monovita <depointon@gmail.com>
2026-06-14 17:50:50 +0300

    HWs 24 and 25

---
+++ tests/logInPositiveTests.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import { describe } from 'node:test';
+import { LogInForm } from "../helpers/Page Objects/logInForm.js";
+import { Credentials } from "../helpers/Page Objects/credentials.js";
+
+const credentials = new Credentials();
+
+test.describe('LogIn positive tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveURL("/");
+  });
+
+  test('User is logedIn', async ({ page }) => {
+    const logInForm = new LogInForm(page);
+    await logInForm.userLogin(page, credentials.userEmail, credentials.userPassword);
+    await expect(page).toHaveURL(`/` + `panel/garage`);
+  });
+});
